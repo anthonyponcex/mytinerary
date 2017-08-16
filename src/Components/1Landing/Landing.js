07ed68cc14a5a8dc2e76360e83dc813825444561
Anthony <anthonyponcex@gmail.com>
2017-08-16 13:33:05 -0700

    hello world displays for landing

---
+++ src/Components/1Landing/Landing.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class Landing extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>Hello World</h1>
+            </div>
+        );
+    }
+}
