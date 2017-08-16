b630bdce731dc34517899602405872f5048118b8
Anthony <anthonyponcex@gmail.com>
2017-08-16 13:47:33 -0700

    add file

--- src/Components/2Journies/Feed.js
+++ src/Components/2Journies/Feed.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
-export default class # extends Component {
+export default class Feed extends Component {
     render() {
         return (
-            
+            <div className="container">
+                <Navbar />
+
+                <div className="row text-center">
+                    Filter by:
+                    <Dropdown />
+                </div>
+
+                <div className="row">
+                    <div className="col-xs-12">
+                        {/*Map over (8) itinerary cards correlated to destination entered*/}
+                        <Card />     
+                    </div>
+                </div>
         );
     }
 }
